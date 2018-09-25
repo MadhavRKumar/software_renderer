ac61a52c473b3d18544c44b8671e6070d9577ade
MadhavRKumar <MadhavRKumar@gmail.com>
2018-09-25 14:15:29 -0400

    minor optimization to line algo

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 #include "tgaimage.cpp"
+#include "geometry.h"
 #include <cstdlib>
 
 const TGAColor white = TGAColor(255, 255,255,255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
-
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 800;
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
 
 int main(int argc, char const *argv[])
 {
-    TGAImage image(100, 100, TGAImage::RGB);
+    TGAImage image(WIDTH, HEIGHT, TGAImage::RGB);
     line(13, 20, 80, 40, image, white);
     line(20, 13, 40, 80, image, white);
 
     image.flip_vertically();
     image.write_tga_file("output.tga");
+
     return 0;
 }
 
@@ -32,10 +35,11 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
         std::swap(y0, y1);
     }
 
-
-    for(float x=x0; x < x1; x++)
+    float denom = (x1- x0);
+    
+    for(float x=x0; x <= x1; x++)
     {
-        float t = (x - x0) / (x1 - x0);
+        float t = (x - x0) / denom;
         int y = y0*(1.0f - t) + y1*t;
         if(steep)
         {
