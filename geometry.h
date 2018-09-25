1cab06ce5cd86ba44c559e92fe2db4af17e3d842
MadhavRKumar <MadhavRKumar@gmail.com>
2018-09-25 14:26:49 -0400

    added generic subtraction, multiplication, and division to vector lib

--- geometry.h
+++ geometry.h
@@ -12,7 +12,9 @@ typedef Vector<2> Vec2;
 typedef Vector<3> Vec3;
 typedef Vector<4> Vec4;
 
- template <int n>  Vector<n> operator+(Vector<n> a, Vector<n> b)
+
+// Addition
+template <int n>  Vector<n> operator+(Vector<n> a, Vector<n> b)
 {
     Vector<n> answer = a;
     for(int i = 0; i < n; i++) 
@@ -21,6 +23,66 @@ typedef Vector<4> Vec4;
     }
 
     return answer;
+};
+
+template <int n>  Vector<n>& operator+=(Vector<n>& a, Vector<n> b)
+{
+    a = a + b;
+    return a;
+};
+
+// Subtraction
+template <int n>  Vector<n> operator-(Vector<n> a, Vector<n> b)
+{
+    Vector<n> answer = a;
+    for(int i = 0; i < n; i++) 
+    {
+        answer.data[i] = a.data[i] - b.data[i];
+    }
+
+    return answer;
+};
+
+template <int n>  Vector<n>& operator-=(Vector<n>& a, Vector<n> b)
+{
+    a = a - b;
+    return a;
+};
+
+// Multiplication
+template <int n>  Vector<n> operator*(Vector<n> a, Vector<n> b)
+{
+    Vector<n> answer = a;
+    for(int i = 0; i < n; i++) 
+    {
+        answer.data[i] = a.data[i] * b.data[i];
+    }
+
+    return answer;
+};
+
+template <int n>  Vector<n>& operator*=(Vector<n>& a, Vector<n> b)
+{
+    a = a * b;
+    return a;
+};
+
+// Division
+template <int n>  Vector<n> operator/(Vector<n> a, Vector<n> b)
+{
+    Vector<n> answer = a;
+    for(int i = 0; i < n; i++) 
+    {
+        answer.data[i] = a.data[i] / b.data[i];
+    }
+
+    return answer;
+};
+
+template <int n>  Vector<n>& operator/=(Vector<n>& a, Vector<n> b)
+{
+    a = a / b;
+    return a;
 };
 
 #endif
